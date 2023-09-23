a9b15ee415bae0d7093c2ad381cf76373f2ce1eb
Abanoub Zaky <zaky.abanoub@gmail.com>
2023-09-24 00:20:51 +0300

    initialize project

---
+++ app/layout.jsx
@@ -0,0 +1,23 @@
+import "@styles/globals.css";
+
+export const metadata = {
+  title: "Promtopia",
+  description: "Discover & Share AI Prompts",
+};
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+        <main className="app">
+          <div>{children}</div>
+        </main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
